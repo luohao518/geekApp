01afc7104bb67e837433445b6257c6c245c51789
saxonnn12@gmail.com <wangyan8>
2018-04-26 09:52:33 +0800

    增加支持tomcat启动方式

--- src/main/java/xyz/geekweb/GeekWebApplication.java
+++ src/main/java/xyz/geekweb/GeekWebApplication.java
@@ -2,6 +2,8 @@ package xyz.geekweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
@@ -11,8 +13,13 @@ import java.io.IOException;
  */
 @SpringBootApplication
 @EnableScheduling
-public class GeekWebApplication {
+public class GeekWebApplication extends SpringBootServletInitializer {
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GeekWebApplication.class);
+    }
 
     public static void main(String[] args) throws IOException {
 
