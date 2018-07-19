e7611eb0f3322360a700cb4643d518fbbb391326
jack.luo <luohao518@yeah.net>
2018-07-19 19:21:32 +0800

    google login

---
+++ geekweb/src/main/java/xyz/geekweb/google/GoogleController.java
@@ -0,0 +1,64 @@
+package xyz.geekweb.google;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import xyz.geekweb.paypal.service.PayPalService;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+/**
+ * @author lhao
+ */
+@Controller
+@RequestMapping("/google")
+public class GoogleController {
+
+    private Logger logger = LoggerFactory.getLogger(GoogleController.class);
+
+    @Autowired
+    private PayPalService payPalService;
+
+    @GetMapping("login")
+    public String login() {
+        logger.debug("do login()");
+        return "google/login";
+    }
+
+    @RequestMapping(value = "/googleVerify", method = RequestMethod.POST)
+    public String verifyToken(String idtokenstr) {
+        System.out.println(idtokenstr);
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
+                new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+                .setAudience(Collections.singletonList("620348788467-kc625s0dfie157c8e0gjnju53njalonj.apps.googleusercontent.com")).build();
+        GoogleIdToken idToken = null;
+        try {
+            idToken = verifier.verify(idtokenstr);
+        } catch (GeneralSecurityException e) {
+            System.out.println("验证时出现GeneralSecurityException异常");
+        } catch (IOException e) {
+            System.out.println("验证时出现IOException异常");
+        }
+        if (idToken != null) {
+            System.out.println("验证成功.");
+            GoogleIdToken.Payload payload = idToken.getPayload();
+            String userId = payload.getSubject();
+			System.out.println("User ID: " + userId);
+			String email = payload.getEmail();
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+
+        return "index";
+    }
+
+
+}
